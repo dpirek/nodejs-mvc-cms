2fcde3779e42266beaa4419fc10be207bac3e1f0
Dave <dpirek@gmail.com>
2023-05-17 09:35:48 -0400

    basic cleanup

--- config.js
+++ config.js
@@ -1,12 +1,9 @@
 exports.config = {
- 
   staticContentPath: __dirname + '/app',
   appPath: __dirname + '/app', 
-
   dbConnection: '',
   dbName: '',
   dbUserName: '',
   dbPassword: '',
-	
   portNumber: 8080
 };
