32b2cc9d2c2a49339b7adade7cbacfd308000ebc
Dave <dpirek@gmail.com>
2023-05-17 12:07:57 -0400

    file cleanup

--- app/server.js
+++ app/server.js
@@ -1,45 +1,38 @@
-// References.
-var http = require('http'),
-    lightnode = require('./lib/lightnode'),
-    c = require('../config'),
-    routes = require('./lib/routes'),
-    file = require('./lib/util.file'),
-    jsonRoutes = require('./lib/json.routes');
+const http = require('http');
+const lightnode = require('./lib/lightnode');
+const c = require('../config');
+const routes = require('./lib/routes');
+const file = require('./lib/util.file');
+const jsonRoutes = require('./lib/json.routes');
 
 // Set a global obj for cross controll reference.
 // Just to keep faithful to the DOM :).
 window = {};
 window.userSession = {};
 
-// Create server.
-var server = new http.Server();
+const server = new http.Server();
 server.listen(c.config.portNumber);
 
-// Website static server.
-var website = new lightnode.FileServer(c.config.staticContentPath);
+const website = new lightnode.FileServer(c.config.staticContentPath);
 
-// Request.
-website.delegateRequest = function(req, resp) {
-	
-	// Make non-case-sensitive.
-	var url = req.url.toLowerCase();
+website.delegateRequest = function(req, res) {
+	const url = req.url.toLowerCase();
 	
   if (url.indexOf('/content') === 0){
   	return website;
   } else if (url.indexOf('/admin') === 0){
   	return website;
   } else if (url.indexOf('/upload') === 0){
-  	return file.upload(req, resp);
+  	return file.upload(req, res);
   } else if (url.indexOf('/json') === 0){
-		return jsonRoutes.get(req, resp);
+		return jsonRoutes.get(req, res);
 	} else {
-		return routes.get(req, resp);
+		return routes.get(req, res);
 	}
 };
 
-// When a request comes to the ip server.
-server.addListener('request', function(req, resp) {
-	website.receiveRequest(req, resp);
+server.addListener('request', function(req, res) {
+	website.receiveRequest(req, res);
 });
 
 console.log('http://localhost:' + c.config.portNumber + '/');
